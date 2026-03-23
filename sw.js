56d5fae8119d98c74723b4efa2913fd429e5c407
josuecsantos <josueccsantos@gmail.com>
2026-03-22 22:21:41 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,38 +1,8 @@
-const CACHE = 'jurisquiz-v2';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
-});
-
+const CACHE = 'jurisquiz-v3';
+const ASSETS = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    caches.match(e.request).then(cached => {
-      const network = fetch(e.request).then(res => {
-        if (res && res.status === 200) {
-          const clone = res.clone();
-          caches.open(CACHE).then(c => c.put(e.request, clone));
-        }
-        return res;
-      }).catch(() => cached);
-      return cached || network;
-    })
-  );
+  if (e.request.method !== 'GET' || e.request.url.includes('supabase')) return;
+  e.respondWith(caches.match(e.request).then(cached => { const net = fetch(e.request).then(res => { if (res && res.status === 200) { caches.open(CACHE).then(c => c.put(e.request, res.clone())); } return res; }).catch(() => cached); return cached || net; }));
 });
